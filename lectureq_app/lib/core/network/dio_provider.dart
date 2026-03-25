778b2e648a96de81b3f88aff25b9614d6ccf5b86
jeongbanseog <wibalislfilask@gmail.com>
2026-03-26 07:20:34 +0900

    feat: Dio 인스턴스 및 Base URL 설정
    #10

---
+++ lectureq_app/lib/core/network/dio_provider.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'dio_provider.g.dart';
+
+@riverpod
+Dio dio(Ref ref) {
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: 'http://localhost:8080',
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 10),
+      contentType: 'application/json',
+    ),
+  );
+
+  dio.interceptors.add(
+    LogInterceptor(requestBody: true, responseBody: true),
+  );
+
+  return dio;
+}
