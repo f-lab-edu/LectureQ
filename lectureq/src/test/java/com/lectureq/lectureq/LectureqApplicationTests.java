89f0f4c3ea66935d15f3b21f49da79a4924e1e01
jeongbanseog <wibalislfilask@gmail.com>
2026-03-26 00:07:06 +0900

    chore: Spring Boot 프로젝트 생성 및 의존성 설정
    #3

---
+++ lectureq/src/test/java/com/lectureq/lectureq/LectureqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lectureq.lectureq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
