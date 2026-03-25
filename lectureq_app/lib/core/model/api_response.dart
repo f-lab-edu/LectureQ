2f98c247d14b724e3e2f209bbc6d3cd0fd398eef
jeongbanseog <wibalislfilask@gmail.com>
2026-03-26 07:21:30 +0900

    feat: 공통 API 응답 모델 구현 #10

---
+++ lectureq_app/lib/core/model/api_response.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'api_response.freezed.dart';
+part 'api_response.g.dart';
+
+@Freezed(genericArgumentFactories: true)
+abstract class ApiResponse<T> with _$ApiResponse<T> {
+  const factory ApiResponse({
+    required int status,
+    required String message,
+    T? data,
+  }) = _ApiResponse<T>;
+
+  factory ApiResponse.fromJson(
+    Map<String, dynamic> json,
+    T Function(Object? json) fromJsonT,
+  ) => _$ApiResponseFromJson(json, fromJsonT);
+}
